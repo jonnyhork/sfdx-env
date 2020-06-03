42a43aa514a09db5003b84571b99ba61981a9a79
Jonny Hork <jhork@salesforce.com>
2020-06-03 10:28:31 -0600

    simplify mv command

--- .devcontainer/installExtension.sh
+++ .devcontainer/installExtension.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-parent_path=parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-mv $parent_path/temp/salesforce.code-builder-vscode-0.0.1 $HOME/.vscode-remote/extensions
+mv /root/workspace/sfdx-env/temp/salesforce.code-builder-vscode-0.0.1 $HOME/.vscode-remote/extensions
